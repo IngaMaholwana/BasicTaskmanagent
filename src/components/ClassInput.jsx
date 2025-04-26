ebb0a73a07411488483c7fae34b9afda80f8d2cb
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-04-26 20:45:28 +0200

    added the count for tasks and delete id on task it self easier than what i was thinking and trying for

--- src/components/ClassInput.jsx
+++ src/components/ClassInput.jsx
@@ -8,25 +8,63 @@ class ClassInput extends Component {
     this.state = {
       todos: ['Just some demo tasks', 'As an example'],
       inputVal: '',
+      editingIndex: null, // Track which todo is being edited
+      editingVal: '',     // Track the value while editing
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleEditChange = this.handleEditChange.bind(this);
+    this.handleEditSubmit = this.handleEditSubmit.bind(this);
   }
 
   handleInputChange(e) {
-    this.setState((state) => ({
-      ...state,
-      inputVal: e.target.value,
-    }));
+    this.setState({ inputVal: e.target.value });
   }
 
   handleSubmit(e) {
     e.preventDefault();
+    const trimmedVal = this.state.inputVal.trim();
+    if (trimmedVal !== '') {
+      this.setState((state) => ({
+        todos: [...state.todos, trimmedVal],
+        inputVal: '',
+      }));
+    }
+  }
+
+  handleDelete(index) {
     this.setState((state) => ({
-      todos: state.todos.concat(state.inputVal),
-      inputVal: '',
+      todos: state.todos.filter((_, i) => i !== index),
     }));
+  }
+
+  handleEdit(index) {
+    this.setState({
+      editingIndex: index,
+      editingVal: this.state.todos[index],
+    });
+  }
+
+  handleEditChange(e) {
+    this.setState({ editingVal: e.target.value });
+  }
+
+  handleEditSubmit(index) {
+    const trimmedVal = this.state.editingVal.trim();
+    if (trimmedVal !== '') {
+      this.setState((state) => {
+        const newTodos = [...state.todos];
+        newTodos[index] = trimmedVal;
+        return {
+          todos: newTodos,
+          editingIndex: null,
+          editingVal: '',
+        };
+      });
+    }
   }
 
   render() {
@@ -34,9 +72,7 @@ class ClassInput extends Component {
       <section>
         {/* eslint-disable-next-line react/prop-types */}
         <h3>{this.props.name}</h3>
-        {/* The input field to enter To-Do's */}
         <form onSubmit={this.handleSubmit}>
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
           <label htmlFor="task-entry">Enter a task: </label>
           <input
             type="text"
@@ -46,14 +82,45 @@ class ClassInput extends Component {
           />
           <button type="submit">Submit</button>
         </form>
+
+        {/* Show count component */}
+        <Count number={this.state.todos.length} />
+
         <h4>All the tasks!</h4>
-        {/* The list of all the To-Do's, displayed */}
         <ul>
-          {this.state.todos.map((todo) => (
-            <li key={todo}>{todo}</li>
+          {this.state.todos.map((todo, index) => (
+            <li key={index}>
+              {this.state.editingIndex === index ? (
+                <>
+                  <input
+                    type="text"
+                    value={this.state.editingVal}
+                    onChange={this.handleEditChange}
+                  />
+                  <button onClick={() => this.handleEditSubmit(index)}>Resubmit</button>
+                </>
+              ) : (
+                <>
+                  {todo}
+                  <button onClick={() => this.handleEdit(index)}>Edit</button>
+                  <button onClick={() => this.handleDelete(index)}>Delete</button>
+                </>
+              )}
+            </li>
           ))}
         </ul>
       </section>
+    );
+  }
+}
+
+// New Count component
+class Count extends Component {
+  render() {
+    return (
+      <div style={{ marginTop: '1rem', fontWeight: 'bold' }}>
+        Total tasks: {this.props.number}
+      </div>
     );
   }
 }
--- src/components/FunctionInpt.jsx
+++ src/components/FunctionInpt.jsx
@@ -1,49 +1,128 @@
-import React, { useState } from 'react';
+/* eslint-disable react/destructuring-assignment */
+import React, { Component } from 'react';
 
-// eslint-disable-next-line react/function-component-definition, react/prop-types
-const FunctionalInput = ({ name }) => {
-  /*
-    We declare two state variables and their setters,
-    one to store the To-Do's
-    and the other to store the value of the input field
-  */
-  const [todos, setTodos] = useState(['Just some demo tasks', 'As an example']);
-  const [inputVal, setInputVal] = useState('');
+class ClassInput extends Component {
+  constructor(props) {
+    super(props);
 
-  const handleInputChange = (e) => {
-    setInputVal(e.target.value);
-  };
+    this.state = {
+      todos: ['Just some demo tasks', 'As an example'],
+      inputVal: '',
+      editingIndex: null, // Track which todo is being edited
+      editingVal: '',     // Track the value while editing
+    };
 
-  const handleSubmit = (e) => {
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleEditChange = this.handleEditChange.bind(this);
+    this.handleEditSubmit = this.handleEditSubmit.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.setState({ inputVal: e.target.value });
+  }
+
+  handleSubmit(e) {
     e.preventDefault();
-    setTodos((todo) => [...todo, inputVal]);
-    setInputVal('');
-  };
+    const trimmedVal = this.state.inputVal.trim();
+    if (trimmedVal !== '') {
+      this.setState((state) => ({
+        todos: [...state.todos, trimmedVal],
+        inputVal: '',
+      }));
+    }
+  }
 
-  return (
-    <section>
-      <h3>{name}</h3>
-      {/* The input field to enter To-Do's */}
-      <form onSubmit={handleSubmit}>
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label htmlFor="task-entry">Enter a task: </label>
-        <input
-          type="text"
-          name="task-entry"
-          value={inputVal}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <h4>All the tasks!</h4>
-      {/* The list of all the To-Do's, displayed */}
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo}>{todo}</li>
-        ))}
-      </ul>
-    </section>
-  );
-};
+  handleDelete(index) {
+    this.setState((state) => ({
+      todos: state.todos.filter((_, i) => i !== index),
+    }));
+  }
 
-export default FunctionalInput;
+  handleEdit(index) {
+    this.setState({
+      editingIndex: index,
+      editingVal: this.state.todos[index],
+    });
+  }
+
+  handleEditChange(e) {
+    this.setState({ editingVal: e.target.value });
+  }
+
+  handleEditSubmit(index) {
+    const trimmedVal = this.state.editingVal.trim();
+    if (trimmedVal !== '') {
+      this.setState((state) => {
+        const newTodos = [...state.todos];
+        newTodos[index] = trimmedVal;
+        return {
+          todos: newTodos,
+          editingIndex: null,
+          editingVal: '',
+        };
+      });
+    }
+  }
+
+  render() {
+    return (
+      <section>
+        {/* eslint-disable-next-line react/prop-types */}
+        <h3>{this.props.name}</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="task-entry">Enter a task: </label>
+          <input
+            type="text"
+            name="task-entry"
+            value={this.state.inputVal}
+            onChange={this.handleInputChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+
+        {/* Show count component */}
+        <Count number={this.state.todos.length} />
+
+        <h4>All the tasks!</h4>
+        <ul>
+          {this.state.todos.map((todo, index) => (
+            <li key={index}>
+              {this.state.editingIndex === index ? (
+                <>
+                  <input
+                    type="text"
+                    value={this.state.editingVal}
+                    onChange={this.handleEditChange}
+                  />
+                  <button onClick={() => this.handleEditSubmit(index)}>Resubmit</button>
+                </>
+              ) : (
+                <>
+                  {todo}
+                  <button onClick={() => this.handleEdit(index)}>Edit</button>
+                  <button onClick={() => this.handleDelete(index)}>Delete</button>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+}
+
+// New Count component
+class Count extends Component {
+  render() {
+    return (
+      <div style={{ marginTop: '1rem', fontWeight: 'bold' }}>
+        Total tasks: {this.props.number}
+      </div>
+    );
+  }
+}
+
+export default ClassInput;
